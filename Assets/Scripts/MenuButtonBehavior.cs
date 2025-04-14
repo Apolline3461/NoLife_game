20502e5e0960c1ef5d06db9ac58fa3315a5a8e01
Clément Lagier <clement.lagier@epitech.eu>
2025-04-14 13:45:28 +0900

    [ADD] Added a background with the 3 times periods, animated the title of the game, and linked to gamescene

--- Assets/Scripts/MenuButtonBehavior.cs
+++ Assets/Scripts/MenuButtonBehavior.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 // This script is for a button behavior of the menu in Unity.
 
@@ -9,6 +11,8 @@ public class ButtonBehavior : MonoBehaviour
     public void LaunchGame()
     {
         Debug.Log("Game launched!");
+        // Load the game scene (assuming it's named "GameScene")
+        SceneManager.LoadScene("SampleScene");
     }
 
     // This method is called when the button is hovered over.
