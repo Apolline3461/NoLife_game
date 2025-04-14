40d9ee9b4ed91f0a34bc2a0b12f2a8d9b346b06b
Clément Lagier <clement.lagier@epitech.eu>
2025-04-14 11:57:55 +0900

    [ADD] Added the script for the menu buttons (launch game,settings, and exit the game)

---
+++ Assets/Scripts/MenuButtonBehavior.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+// This script is for a button behavior of the menu in Unity.
+
+
+public class ButtonBehavior : MonoBehaviour
+{
+    // This method is called when the button is clicked.
+    public void LaunchGame()
+    {
+        Debug.Log("Game launched!");
+    }
+
+    // This method is called when the button is hovered over.
+    public void LaunchSettings()
+    {
+        Debug.Log("Settings launched!");
+    }
+
+    // This method is called when the button is no longer hovered over.
+    public void ExitGame()
+    {
+        Debug.Log("Game exited!");
+        Application.Quit();
+    }
+}
