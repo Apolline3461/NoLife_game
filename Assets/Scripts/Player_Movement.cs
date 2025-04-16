b0454d0688a754c45e8cbcf1c2b52b288b5829ae
Apolline <apolline.fontaine@epitech.eu>
2025-04-16 09:24:35 +0400

    update - handle left direction with walk animation

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,20 +6,27 @@ public class Player : MonoBehaviour
     private Rigidbody2D body;
     private bool grounded;
     private Animator animator;
+    private Transform playerTransform;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        playerTransform = transform;
     }
 
-    private void Update()
-    {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
-
+    private void Update() {
         float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
         animator.SetFloat("speed", Mathf.Abs(horizontalInput));
+
+        if (horizontalInput != 0)
+        {
+            Vector3 scale = playerTransform.localScale;
+            scale.x = Mathf.Sign(horizontalInput);
+            playerTransform.localScale = scale;
+        }
 
         if (Input.GetKey(KeyCode.Space) && grounded)
             Jump();
