1ac75711e07215e2ef0534d135323544e4c4d82b
Apolline <apolline.fontaine@epitech.eu>
2025-04-16 14:04:13 +0400

    add - handle jump for each time period, use key ',' to switch between the different time laps

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -1,25 +1,63 @@
 using UnityEngine;
 
+public enum TimePeriod
+{
+    Past,
+    Present,
+    Future
+}
+
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TimePeriod timePeriod;
+
+    private int jumpsLeft;
     private Rigidbody2D body;
     private bool grounded;
     private Animator animator;
     private Transform playerTransform;
+    private CapsuleCollider2D capsuleCollider;
 
     private void Awake()
     {
+        Debug.Log("Script démarré. Période actuelle : " + timePeriod);
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         playerTransform = transform;
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+
+        ApplyTimePeriodSettings();
+    }
+
+    private void ApplyTimePeriodSettings()
+    {
+        switch (timePeriod)
+        {
+            case TimePeriod.Past:
+                body.gravityScale = 5f;
+                jumpForce = 6f;
+                animator.speed = 1.5f;
+                break;
+            case TimePeriod.Present:
+                body.gravityScale = 3.5f;
+                jumpForce = 7f;
+                animator.speed = 1f;
+                break;
+            case TimePeriod.Future:
+                body.gravityScale = 2f;
+                jumpForce = 9f;
+                animator.speed = 0.85f;
+                jumpsLeft = 2;
+                break;
+        }
     }
 
     private void Update() {
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-
-        animator.SetFloat("speed", Mathf.Abs(horizontalInput));
 
         if (horizontalInput != 0)
         {
@@ -28,19 +66,53 @@ public class Player : MonoBehaviour
             playerTransform.localScale = scale;
         }
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        animator.SetFloat("speed", Mathf.Abs(horizontalInput));
+        grounded = IsGrounded();
+
+        if (grounded)
+        {
+            jumpsLeft = (timePeriod == TimePeriod.Future) ? 2 : 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0)
+        {
             Jump();
+        }
+
+        animator.SetBool("isJumping", !grounded);
+
+        foreach (KeyCode kcode in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(kcode))
+                Debug.Log("KeyCode down: " + kcode);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            // Incrémente l'enum en boucle
+            timePeriod = (TimePeriod)(((int)timePeriod + 1) % System.Enum.GetValues(typeof(TimePeriod)).Length);
+            ApplyTimePeriodSettings();
+            Debug.Log("TimePeriod changed to: " + timePeriod);
+        }
     }
 
-    private void Jump()
-    {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        grounded = false;
+    private void Jump() {
+        body.velocity = new Vector2(body.velocity.x, jumpForce);
+        jumpsLeft--;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private bool IsGrounded()
     {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
+        float extraHeight = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.CapsuleCast(
+            capsuleCollider.bounds.center,
+            capsuleCollider.bounds.size,
+            capsuleCollider.direction,
+            0f,
+            Vector2.down,
+            extraHeight,
+            groundLayer
+        );
+        return raycastHit.collider != null;
     }
 }
